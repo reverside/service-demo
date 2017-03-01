f5a28f195e991afea23652e1e86cc95db0dc1a97
mouzie <moses.mathebula16@gmail.com>
2017-03-01 11:19:09 +0200

    Create and FindAll methods.

--- src/main/java/za/co/reverside/demo/Service.java
+++ src/main/java/za/co/reverside/demo/Service.java
@@ -2,12 +2,30 @@ package za.co.reverside.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class Service{
+
+private	List<Car> cars = new ArrayList<Car>();
 
 	@RequestMapping(value="api/hello")
 	public String hello(){
 		return "Hello world";
 	}
+
+	@RequestMapping(path="api/cars", method=RequestMethod.POST, consumes="application/json")
+	public void createCars(@RequestBody Car car){
+		this.cars.add(car);
+	}
+
+	@RequestMapping(path="api/cars", method=RequestMethod.GET, produces="application/json")
+	public List<Car> findAll(){
+		return this.cars;
+	}
+
 }
