e29ad04b39b712e6a17d18b2a0755288ab6801a7
mouzie <moses.mathebula16@gmail.com>
2017-03-02 10:57:24 +0200

    Changes made on Update method today for testing

---
+++ src/main/java/za/co/reverside/demo/Service.java
@@ -0,0 +1,68 @@
+package za.co.reverside.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class Service{
+
+private	List<Car> cars = new ArrayList<Car>();
+
+	@RequestMapping(value="api/hello")
+	public String hello(){
+		return "Hello world";
+	}
+
+	@RequestMapping(path="api/cars", method=RequestMethod.POST, consumes="application/json")
+	public void createCars(@RequestBody Car car){
+		this.cars.add(car);
+	}
+
+	@RequestMapping(path="api/cars", method=RequestMethod.GET, produces="application/json")
+	public List<Car> findAll(){
+		return this.cars;
+	}
+
+	@RequestMapping(path="api/cars/{id}", method=RequestMethod.GET, produces="application/json")
+	public Car findOne(@PathVariable("id") String id){
+		Car car = null;
+
+		for(Car aCar: cars){
+			if(aCar.getId().equals(id))
+				car = aCar;
+		}
+		return car;
+	}
+
+	@RequestMapping(path="api/cars/{id}", method=RequestMethod.DELETE)
+	public void deleteOne(@PathVariable("id") String id){
+		
+		Car car = null;
+
+		for(Car aCar: cars){
+			if(aCar.getId().equals(id))
+				car = aCar;
+		}
+		this.cars.remove(car);
+	}
+
+	@RequestMapping(path="api/cars/{id}", method=RequestMethod.PUT, produces="application/json")
+	public void updateOne(@RequestBody Car car, @PathVariable("id") String id){
+
+		for(Car aCar: cars){
+			if(aCar.getId().equals(id)){
+				car = aCar;
+				this.cars.set(id,car);
+			}
+			
+		}
+		
+	}
+
+}
