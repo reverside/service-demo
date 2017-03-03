56576853435b4899c507b8afff01bb1212c505d0
Mouzie <moses.mathebula16@gmail.com>
2017-03-03 04:09:08 -0800

    Angular JS

--- src/main/java/za/co/reverside/demo/Application.java
+++ src/main/java/za/co/reverside/demo/Application.java
@@ -3,6 +3,13 @@ package za.co.reverside.demo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 @SpringBootApplication
 public class Application{
 
@@ -11,4 +18,14 @@ public class Application{
 
 		SpringApplication.run(Application.class, args);
 	} 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:9000");
+            }
+        };
+    }
+
 }
