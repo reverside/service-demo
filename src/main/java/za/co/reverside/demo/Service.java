2ac60a280ed7261474f3f1cf314ac819a05c2f3e
Kanya Msila <kanya.msila@gmail.com>
2017-03-07 14:37:47 +0200

    changed the pom

--- src/main/java/za/co/reverside/demo/Service.java
+++ src/main/java/za/co/reverside/demo/Service.java
@@ -5,17 +5,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
+import za.co.reverside.demo.repository.UserRepository;
+import za.co.reverside.demo.model.User;
 
 @RestController
 public class Service{
+
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping(value="api/hello")
 	public String hello(){
 		return "Hello world";
 	}
+	
 	
 
 }
