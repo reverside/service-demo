6657d6ffd40a832f1e7faa91782f4673bc54886d
Mouzie <moses.mathebula16@gmail.com>
2017-03-08 14:51:03 +0200

    Import not used removed.

--- src/main/java/za/co/reverside/demo/Service.java
+++ src/main/java/za/co/reverside/demo/Service.java
@@ -2,12 +2,6 @@ package za.co.reverside.demo;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class Service{
