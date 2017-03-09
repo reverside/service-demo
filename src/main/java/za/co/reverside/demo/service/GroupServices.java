fc5a69ec16b2159de43090b3abeca193437f0f06
Mouzie <moses.mathebula16@gmail.com>
2017-03-09 11:21:01 +0200

    Service group created

--- src/main/java/za/co/reverside/demo/service/GroupServices.java
+++ src/main/java/za/co/reverside/demo/service/GroupServices.java
@@ -2,6 +2,7 @@ package za.co.reverside.demo.service;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,8 +26,39 @@ public class GroupServices {
     public void createGroup(@RequestBody Group group){
         this.groupRepository.insert(group);
     }
+    
     @RequestMapping(path="api/groups", method = RequestMethod.GET, produces = "application/json")
     public List<Group> findGroups(){
         return this.groupRepository.findAll();
+    }
+    
+    @RequestMapping(path="api/groups/{groupId}", method = RequestMethod.GET, produces = "application/json")
+    public Group findGroup(@PathVariable("groupId") String id ){
+            return this.groupRepository.findOne(id);
+    }
+    
+        
+    @RequestMapping(path="api/groups/{groupId}", method = RequestMethod.PUT, consumes = "application/json")
+    public void updateGroup(@PathVariable  String groupId, @RequestBody Group request){
+        Group group = this.groupRepository.findOne(groupId);
+        if(group != null){
+            group.setName(request.getName());
+            group.setDescription(request.getDescription());
+            groupRepository.save(group);
+        }else
+        {
+            throw new RuntimeException ("No such group");
+        }
+        
+    }
+    
+    @RequestMapping(path="api/groups/{groupId}", method = RequestMethod.DELETE)
+    public void deleteGroup(@PathVariable("groupId") String id){
+        Group group = this.groupRepository.findOne(id);
+        if( group == null){
+            throw new RuntimeException ("No group found");
+        }else{
+            groupRepository.delete(id);
+        }
     }
 }
